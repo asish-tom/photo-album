a132f610ff0574c47591e4062da9d0c0acfa9476
asish-tom <asishtom7@gmail.com>
2020-09-14 00:08:19 +0530

    1.Code cleanup 2.Some swagger fixes. 3.Bit of refactoring.

--- helpers/response.go
+++ helpers/response.go
@@ -1,31 +1,20 @@
 package helpers
 
-// Todo-> Refine response wrapper
 import (
 	"encoding/json"
 	"net/http"
 )
 
-// Main wrapper for response
-//
-// swagger:response ResponseWrapper
-type ResponseWrapper struct {
-	// in:body
-	Body Response
-}
-
+// swagger:response ResponseModel
 // Wrapper for all responses form the system
-//
-type Response struct {
-	// Status of the request
-	Status string `json:"status"`
-	// Description on what happened to the request
-	Message string `json:"message"`
-	// Data returned from API as part of request
-	Model interface{} `json:"model"`
+type ResponseModel struct {
+	// in: body
+	Status  string
+	Message string
+	Model   interface{}
 }
 
-func (r *Response) ToResponse(writer http.ResponseWriter) {
+func (r *ResponseModel) ToResponse(writer http.ResponseWriter) {
 	e := json.NewEncoder(writer)
 	err := e.Encode(r)
 	if err != nil {
